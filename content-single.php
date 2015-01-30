cb55336543d62a5c30a2155ea6cefc5bdb86ad3d
pjsinco <psinco@fas.harvard.edu>
2015-01-29 20:23:44 -0600

    tries picturefill with photo of dr. taylor

--- content-single.php
+++ content-single.php
@@ -10,7 +10,7 @@
                 // let's get the img src of the featured image
                 $img_src = wp_get_attachment_url(get_post_thumbnail_id()); 
               ?>
-              <img class="image__img" src="<?php echo $img_src; ?>" <?php echo tevkori_get_src_sizes( 179419, 'small' ); ?> />
+              <img class="image__img" src="http://elit.dev/wp-content/uploads/2015/01/taylor.jpg" <?php echo tevkori_get_src_sizes( 179421, 'article-top-large' ); ?> />
             </picture>
             <figcaption class="image__caption caption caption--feature caption--right">
               Sed Consectetur Lobortis, LL
