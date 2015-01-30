3670aca43a877c6c9116bd5d9a318692af13cd70
pjsinco <psinco@fas.harvard.edu>
2015-01-30 11:33:35 -0600

    adds markup with static copy for header info in content-single

--- content-single.php
+++ content-single.php
@@ -5,6 +5,8 @@
 ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
           <figure class="image--primary">
+
+<!-- removing picture element because it's not used by ricg picturefill plugin -->
 <!--             <picture> -->
               <?php 
                 // let's get the img src of the featured image
@@ -13,9 +15,32 @@
               <img class="image__img" src="<?php echo $img_src; ?>" <?php echo tevkori_get_src_sizes( 179421, 'article-top-large' ); ?> />
 <!--             </picture> -->
             <figcaption class="image__caption caption caption--feature caption--right">
-              Sed Consectetur Lobortis, LL
+              <?php  ?>
             </figcaption>
           </figure>
           <div class="story">
+            <header class="story-header">
+              <h5 class="story-header__kicker">Duis Arte</h5>
+              <h1 class="story-header__title">Media 101: How to position yourself as a medical expert</h1>
+              <h3 class="story-header__teaser">Do you see yourself as the next Dr. Sanjay Gupta? A seasoned expert shares words of wisdom on working with the media.</h3>
+              <div>
+                <div class="story-meta">
+                  <span class="story-meta__by">By</span> 
+                  <span class="story-meta__author">Euismod Mollis Purus</span> 
+                  <span class="story-meta__date">Wednesday, Dec. 14, 2014</span>
+                  <span class="meta__comment-link comment-link">
+                    <a href="#comments" class="comment-link__link">
+                      <span class="icon-comment">
+                        <span class="text-replace">Comments</span>
+                      </span> 
+                      <span class="comment-link__body">5</span>
+                    </a>
+                  </span>
+                </div> <!-- story-meta -->
+              </div>
 
+
+
+
+            </header>
 
--- single.php
+++ single.php
@@ -7,7 +7,6 @@
 
         <?php while(have_posts()): the_post(); ?>
 
-
           <?php get_template_part('content', 'single'); ?>
 
           <?php 
