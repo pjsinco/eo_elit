b270804afbd9ea52f6bd6becd39f26a89ab04a64
pjsinco <psinco@fas.harvard.edu>
2015-02-03 13:37:57 -0600

    pull about-the-author info, add to post

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -92,18 +92,32 @@ function elit_story_footer() {
     printf('<div class="story-nav">%1$s</div>', $tags_list);
   }
 
-  // #4 output our about-the-author
+  // #4 output our about-the-author if we have the info
+  $author_bio = get_post_meta( get_the_id(), 'elit_bio', true );
+  if ( $author_bio ) {
+    $about  = '<div class="story-footer__section">';
+    $about .= '<div class="story-footer__title">About the author</div>';
 
-  $about  = '<div class="story-footer__section">';
-  $about .= '<div class="story-footer__title">About the author</div>';
-  //$about .= '<img src="img/milam112.jpg" alt="Elit Commodo Purus" width="95px" class="story-footer__img">'
-  //<div class="story-footer__body">Elit Commodo Purus, OMS IV, dolor sit amet, consectetur adipiscing elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</div>
-//</div>
+    // get the id of the author's photo
+    $author_photo_id = get_post_meta( get_the_id(), 'elit_author_photo_id', true );
 
-              //<div class="story-footer__section">
-                //<div class="story-footer__title">Credit</div>
-                //<div class="story-footer__body">Top of page: Photo of Dr. Caudle by <a href="#">Tristique Cras</a>.</div>
-              //</div>
+    if ( $author_photo_id ) {
+      $author_photo_src = wp_get_attachment_url( $author_photo_id );
+      $author_photo_content = get_post( $author_photo_id );
+      $about .= '<img class="story-footer__img" ';
+      $about .= 'src="' . wp_get_attachment_url( $author_photo_id ) . '" ';
+      $about .= 'alt = "' . $author_photo_content->post_excerpt . '" '  ;
+      $about .= ' width="' . wp_get_attachment_image_src($author_photo_id)[1] . '" />';
+    }
+    
+    $about .= '<div class="story-footer__body">';
+    $about .= $author_bio;
+    $about .= '</div></div>';
+
+    echo $about;
+  }
+
+  
               //<div class="story-nav__more-in">More in <a href="#" class="story-nav__emph">Patient Care <span class="icon-arrow-right"></span></a></div>
               //<div class="prev-next">
                 //<div class="prev-next__next"><a href="#" class="prev-next__title">Newer <span class="icon-arrow-right space-to-left"></span></a><a href="#" class="prev-next__link">New Year’s resolutions: 10 life hacks to increase physician productivity </a></div>
