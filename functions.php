ee34219b1154fa503a3fa95ca9dc289a89f21ab8
pjsinco <psinco@gmail.com>
2015-02-10 11:22:06 -0600

    bruce adds a few update_option function calls

--- functions.php
+++ functions.php
@@ -1,8 +1,13 @@
 <?php 
-
 /*
  * elit functions and defs
  */
+
+  /* Change the Site URL */
+  /*
+  update_option('siteurl','http://thedodev.osteopathic.org');
+  update_option('home','http://thedodev.osteopathic.org');
+  */
 
 // temporarily disable admin bar so we can see our susy grid display
 //add_filter('show_admin_bar', '__return_false');
