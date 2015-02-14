122f81bf7218fabc0f16156d094003a57f56c679
pjsinco <psinco@fas.harvard.edu>
2015-02-14 10:22:56 -0600

    add featurable meta box

--- inc/elit-meta-boxes.php
+++ inc/elit-meta-boxes.php
@@ -420,3 +420,83 @@ function elit_save_featured_video_meta( $post_id, $post ) {
   }
 }
 
+/**
+ * FEATURABLE META BOX
+ *
+ */
+add_action( 'load-post.php', 'elit_featurable_meta_box_setup' );
+add_action( 'load-post-new.php', 'elit_featurable_meta_box_setup' );
+
+function elit_featurable_meta_box_setup() {
+  add_action( 'add_meta_boxes', 'elit_add_featurable_meta_box' );
+  add_action( 'save_post', 'elit_save_featurable_meta', 10, 2 );
+}
+
+function elit_add_featurable_meta_box() {
+  add_meta_box(
+    'elit-featurable',
+    esc_html( 'Not featurable' ),
+    'elit_featurable_meta_box',
+    'post',
+    'side',
+    'low'
+  );
+}
+
+function elit_featurable_meta_box( $object, $box ) {
+  wp_nonce_field( basename(__FILE__), 'elit_featurable_nonce' );
+
+  $featurable = get_post_meta( $object->ID, 'elit_featurable', true );
+
+  ?>
+  <p>
+    <label for="widefat">This story is not a feature.</label>
+    <br />
+    <br />
+    <input class="widefat" type="checkbox" name="elit-featurable" id="elit-featurable" value="no" <?php if ( $featurable ): checked( $featurable, 'no' ); endif; ?>" />
+    Unfeature
+  </p>
+  <?php 
+}
+
+function elit_save_featurable_meta( $post_id, $post ) {
+  // verify the nonce
+  if ( !isset( $_POST['elit_featurable_nonce'] ) || 
+    !wp_verify_nonce( $_POST['elit_featurable_nonce'], basename( __FILE__ ) )
+  ) {
+      // instead of just returning, we return the $post_id
+      // so other hooks can continue to use it
+      return $post_id;
+  }
+
+  // get post type object
+  $post_type = get_post_type_object( $post->post_type );
+
+  // if the user has permission to edit the post
+  if ( !current_user_can( $post_type->cap->edit_post, $post_id ) ) {
+    return $post_id;
+  }
+
+  // get the posted data and sanitize it
+  $new_meta_value = 
+    ( isset($_POST['elit-featurable'] ) ? $_POST['elit-featurable'] : '' );
+
+  // set the meta key
+  $meta_key = 'elit_featurable';
+
+  // get the meta value as a string
+  $meta_value = get_post_meta( $post_id, $meta_key, true);
+
+  // if a new meta value was added and there was no previous value, add it
+  if ( $new_meta_value && $meta_value == '' ) {
+    //add_post_meta( $post_id, 'elit_foo', 'bar');
+    add_post_meta( $post_id, $meta_key, $new_meta_value, true);
+  } elseif ($new_meta_value && $new_meta_value != $meta_value ) {
+    // so the new meta value doesn't match the old one, so we're updating
+    update_post_meta( $post_id, $meta_key, $new_meta_value );
+  } elseif ( $new_meta_value == '' && $meta_value) {
+    // if there is no new meta value but an old value exists, delete it
+    delete_post_meta( $post_id, $meta_key, $meta_value );
+  }
+}
+
