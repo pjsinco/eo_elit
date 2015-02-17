2b01ac9d26fc39afa29b0addb23c73cda0bf3873
pjsinco <psinco@fas.harvard.edu>
2015-02-17 17:22:27 -0600

    bring in static markup for social-pick

--- front-elit_social_pick.php
+++ front-elit_social_pick.php
@@ -19,3 +19,23 @@
   d( $social_pick_meta );
 
 ?>
+      <div class="row">
+        <div class="size-1-of-1 social-pick-red module">
+          <div class="social-pick-red__date">Jan. 15 on Twitter</div>
+          <div class="social-pick-red__body">
+            <div class="social-pick-red__img"><img src="img/minoritydoctor-bigger.jpg" class="image__img"></div>
+            <div class="social-pick-red__author"><a href="#" class="social-pick-red__link">@minoritydoctor</a></div>
+            <p class="social-pick-red__body-text">Done with all my exams for this week, so now I finally have time to study for next week's big exam :-/ <a href="#" class="social-pick-red__link">#medschoolproblems</a>
+            </p>
+          </div>
+          <div class="social-pick-red__note"> 
+            <ul class="social"> 
+              <li class="social__icon"><a href="https://www.facebook.com/americanosteopathicassociation" class="social__link"><span class="icon-facebook"><span class="text-replace">Facebook</span></span></a></li>
+              <li class="social__icon"><a href="https://twitter.com/TheDOmagazine" class="social__link"><span class="icon-twitter"><span class="text-replace">Twitter</span></span></a></li>
+              <li class="social__icon"><a href="https://www.linkedin.com/groups/American-Osteopathic-Association-76192" class="social__link"><span class="icon-linkedin"><span class="text-replace">LinkedIn</span></span></a></li>
+              <li class="social__icon"><a href="http://www.pinterest.com/AOAHealth/" class="social__link"><span class="icon-pinterest"><span class="text-replace">Pinterest</span></span></a></li>
+            </ul>
+            <p>Follow us </p>
+          </div>
+        </div> <!-- .size-1-of-1 .social-pick-red .module -->
+      </div> <!-- .row -->
