29485d740c11522aad832edd74c8a343631f5312
pjsinco <psinco@fas.harvard.edu>
2015-02-21 08:12:34 -0600

    bring in static code to stub out spotlight

---
+++ front-elit_spotlight.php
@@ -0,0 +1,29 @@
+<?php 
+/**
+ * Template for the front-page spotlight.
+ * 
+ *
+ * @package elit
+ */
+
+?>
+
+      <div class="row">
+        <div class="size-1-of-1">
+          <div class="section-title-hat"><span class="section-title-hat__text">Spotlight</span></div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="unit size-1-of-1 module">
+          <div id="spotlight" class="spotlight">
+            <div class="spotlight__feature-wrapper">
+              <iframe width="655" height="400" src="//www.youtube.com/embed/SbTcdzb1HR0?color=white&amp;autohide=1&amp;modestbranding=1" frameborder="0" allowfullscreen></iframe>
+            </div>
+            <div class="spotlight__body">
+              <h5 class="spotlight__kicker">Egestas Purus</h5>
+              <h2 class="spotlight__head">Oras justo odio, dapibus ac facilisis in, egestas eget quam.</h2>
+              <p class="spotlight__body-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod.</p>
+            </div>
+          </div>
+        </div>
+      </div>
