640bfe9504877e2a8256bdf3d4876dbf3d822632
pjsinco <psinco@fas.harvard.edu>
2015-02-25 11:59:18 -0600

    bring in widget in article-sidebar, if one's there

--- sidebar-article.php
+++ sidebar-article.php
@@ -10,3 +10,5 @@
         <aside data-set="rover-don-parent" class="ad rover-don-parent-b"></aside>
         <aside data-set="rover-peggy-parent" class="ad rover-peggy-parent-b"></aside>
 
+        <?php if ( !dynamic_sidebar('article-sidebar') ); ?>
+
