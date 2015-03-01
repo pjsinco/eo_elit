f30017eb402d183fdbc28d199ef6602b202600e4
pjsinco <psinco@fas.harvard.edu>
2015-03-01 13:54:05 -0600

    add the minuscule beginnings of a footer

--- footer.php
+++ footer.php
@@ -1,4 +1,18 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package elit
+ */
+?>
 
+	  <footer id="colophon" class="footer" role="contentinfo">
+	  	<div class="site-info">
+        Footer stuff will go here
+	  	</div><!-- .site-info -->
+	  </footer><!-- #colophon -->
     <script src="http://localhost:35729/livereload.js"></script>
     <?php wp_footer(); ?>
   </body>
