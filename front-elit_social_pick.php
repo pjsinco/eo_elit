caffc0e9bce7db9906b2ea367b63a37f77dc6d23
pjsinco <psinco@fas.harvard.edu>
2015-03-03 15:30:25 -0600

    add target=_blank to follow-us links on front page

--- front-elit_social_pick.php
+++ front-elit_social_pick.php
@@ -14,8 +14,6 @@
 
   if ( $social_pick ): $meta = get_post_meta( $social_pick->ID ); endif;
 
-  d( $social_pick );
-  d( $meta );
   $args = array(
     'class' => 'image__img',
   );
@@ -32,10 +30,10 @@
           </div>
           <div class="social-pick-red__note"> 
             <ul class="social"> 
-              <li class="social__icon"><a href="https://www.facebook.com/americanosteopathicassociation" class="social__link"><span class="icon-facebook"><span class="text-replace">Facebook</span></span></a></li>
-              <li class="social__icon"><a href="https://twitter.com/TheDOmagazine" class="social__link"><span class="icon-twitter"><span class="text-replace">Twitter</span></span></a></li>
-              <li class="social__icon"><a href="https://www.linkedin.com/groups/American-Osteopathic-Association-76192" class="social__link"><span class="icon-linkedin"><span class="text-replace">LinkedIn</span></span></a></li>
-              <li class="social__icon"><a href="http://www.pinterest.com/AOAHealth/" class="social__link"><span class="icon-pinterest"><span class="text-replace">Pinterest</span></span></a></li>
+              <li class="social__icon"><a href="https://www.facebook.com/americanosteopathicassociation" class="social__link" target="_blank"><span class="icon-facebook"><span class="text-replace">Facebook</span></span></a></li>
+              <li class="social__icon"><a href="https://twitter.com/TheDOmagazine" class="social__link" target="_blank"><span class="icon-twitter"><span class="text-replace">Twitter</span></span></a></li>
+              <li class="social__icon"><a href="https://www.linkedin.com/groups/American-Osteopathic-Association-76192" class="social__link" target="_blank"><span class="icon-linkedin"><span class="text-replace">LinkedIn</span></span></a></li>
+              <li class="social__icon"><a href="http://www.pinterest.com/AOAHealth/" class="social__link" target="_blank"><span class="icon-pinterest"><span class="text-replace">Pinterest</span></span></a></li>
             </ul>
             <p>Follow us </p>
           </div>
