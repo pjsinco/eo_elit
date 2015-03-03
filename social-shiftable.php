bc6bd46c4ab0653e651ca5e19c21ab49998e29cc
pjsinco <psinco@fas.harvard.edu>
2015-03-03 10:21:49 -0600

    add let's-try-it facebook link to facebook share icon

--- social-shiftable.php
+++ social-shiftable.php
@@ -1,6 +1,20 @@
+<?php 
+/**
+ * 
+ * Template for displaying social icons
+ * 
+ * @package elit
+ */
+
+
+?>
+
+<?php 
+  $link = urlencode( get_permalink() );
+?>
 <ul class="social social--shiftable">
   <li class="social__icon">
-    <a href="#" class="social--shiftable__link">
+    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $link; ?>" class="social--shiftable__link" target="_blank">
       <span class="icon-facebook">
         <span class="text-replace">Facebook</span>
       </span>
