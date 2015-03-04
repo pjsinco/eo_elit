c4ba3175cd3cc7df9f3c8c797a909fa4217ab01d
pjsinco <psinco@fas.harvard.edu>
2015-03-03 18:09:27 -0600

    stub out footer

--- footer.php
+++ footer.php
@@ -9,9 +9,62 @@
 ?>
 
 	  <footer id="colophon" class="footer" role="contentinfo">
-	  	<div class="site-info">
-        Footer stuff will go here
-	  	</div><!-- .site-info -->
+      <div class="row">
+        <div class="size-1-of-3">
+          <h3 class="footer_title">About the AOA</h3>
+          <ul class="footer__list">
+            <li class="footer__list-item">
+              <a class="footer__link" href="#">AOA Membership</a>
+            </li>
+          </ul>
+          <h3 class="footer_title">Accreditation</h3>
+          <ul class="footer__list">
+            <li class="footer__list-item">
+              <a class="footer__link" href="#">AOA Membership</a>
+            </li>
+            
+          </ul>
+        </div>
+        <div class="size-1-of-3">
+          <h3 class="footer_title">Advocacy</h3>
+          <ul class="footer__list">
+            <li class="footer__list-item">
+              <a class="footer__link" href="#">AOA Membership</a>
+            </li>
+            
+          </ul>
+          <h3 class="footer_title">Education</h3>
+          <ul class="footer__list">
+            <li class="footer__list-item">
+              <a class="footer__link" href="#">AOA Membership</a>
+            </li>
+            
+          </ul>
+        </div>
+        <div class="size-1-of-3--last">
+          <h3 class="footer_title">DO Profiles</h3>
+          <ul class="footer__list">
+            <li class="footer__list-item">
+              <a class="footer__link" href="#">AOA Membership</a>
+            </li>
+            
+          </ul>
+          <h3 class="footer_title">Professional Development</h3>
+          <ul class="footer__list">
+            <li class="footer__list-item">
+              <a class="footer__link" href="#">AOA Membership</a>
+            </li>
+            
+          </ul>
+          <h3 class="footer_title">Other publications</h3>
+          <ul class="footer__list">
+            <li class="footer__list-item">
+              <a class="footer__link" href="#">AOA Membership</a>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
 	  </footer><!-- #colophon -->
     <script src="http://localhost:35729/livereload.js"></script>
     <?php wp_footer(); ?>
