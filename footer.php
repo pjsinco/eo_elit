640bbf3a330867458f96c3dd109f301991976baf
pjsinco <psinco@fas.harvard.edu>
2015-03-04 05:38:49 -0600

    style footer

--- footer.php
+++ footer.php
@@ -10,132 +10,132 @@
 
     
       <footer id="colophon" class="footer" role="contentinfo">
-        <div class="row module">
-          <div class="size-1-of-3">
-            <h3 class="footer_title">About the AOA</h3>
-            <ul class="footer__list">
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/about/aoa-membership/Pages/default.aspx" title="Enjoy the Benefits of Being an AOA Member">AOA Membership</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/about/leadership/Pages/default.aspx" title="Leadership & Policy">Leadership and Policy</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/about/affiliates/Pages/default.aspx" title="AOA Affiliates, Partnerships, and Teams">Related Organizations</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.aoastore.com/RetailSite.asp" title="American Osteopathic Association">AOA Store</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/about/Pages/osteopathic-family-relief-fund.aspx" title="Osteopathic Family Relief Fund">Osteopathic Family Relief Fund</a>
-              </li>
-            </ul>
-            
-            <h3 class="footer_title">Accreditation</h3>
-            <ul class="footer__list">
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/accreditation/predoctoral%20accreditation/Pages/default.aspx" title="Predoctoral Accreditation">Predoctoral Accreditation</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/accreditation/postdoctoral-training-approval/Pages/default.aspx" title="Postdoctoral Training Approval">Postdoctoral Training Approval</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/accreditation/opti-approval/Pages/default.aspx" title="Osteopathic Postdoctoral Training Institution Approval">OPTI Approval</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.hfap.org/" title="HFAP">Healthcare Facilities Accreditation Program</a>
-              </li>
-            </ul>
-          </div> <!-- .size-1-of-3 -->
+        <div class="footer__body">
+          <div class="row--full">
+            <h2 class="footer__header">More from the <span>American Osteopathic Association</span></h2>
+            <div class="footer__col">
+              <h3 class="footer__title">About the AOA</h3>
+              <ul class="footer__list">
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/about/aoa-membership/Pages/default.aspx" title="Enjoy the Benefits of Being an AOA Member">AOA Membership</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/about/leadership/Pages/default.aspx" title="Leadership & Policy">Leadership and Policy</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/about/affiliates/Pages/default.aspx" title="AOA Affiliates, Partnerships, and Teams">Related Organizations</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.aoastore.com/RetailSite.asp" title="American Osteopathic Association">AOA Store</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/about/Pages/osteopathic-family-relief-fund.aspx" title="Osteopathic Family Relief Fund">Osteopathic Family Relief Fund</a>
+                </li>
+              </ul>
+              
+              <h3 class="footer__title">Accreditation</h3>
+              <ul class="footer__list">
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/accreditation/predoctoral%20accreditation/Pages/default.aspx" title="Predoctoral Accreditation">Predoctoral Accreditation</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/accreditation/postdoctoral-training-approval/Pages/default.aspx" title="Postdoctoral Training Approval">Postdoctoral Training Approval</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/accreditation/opti-approval/Pages/default.aspx" title="Osteopathic Postdoctoral Training Institution Approval">OPTI Approval</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.hfap.org/" title="HFAP">Healthcare Facilities Accreditation Program</a>
+                </li>
+              </ul>
+            </div> <!-- .footer__col -->
 
-          <div class="size-1-of-3">
-            <h3 class="footer_title">Advocacy</h3>
-            <ul class="footer__list">
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/public-policy/federal-legislative-priorities/Pages/default.aspx" title="Federal Legislative Priorities">Legislative Priorities</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/public-policy/regulatory-issues/Pages/default.aspx" title="Regulatory Issues">Regulatory Issues</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/advocacy/Pages/do-advocacy-action-center.aspx" title="Write to Congress">Write to Congress</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/public-policy/state-government-affairs/Pages/default.aspx" title="State Government Affairs">State Government Affairs</a>
-              </li>
-            </ul>
-            
-            <h3 class="footer_title">Education</h3>
-            <ul class="footer__list">
-              <li class="footer__list-item">
-                <a href="http://opportunities.osteopathic.org/index.htm" title="Opportunities.osteopathic.org">Find a Training Program</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://cf.osteopathic.org/iLearn/home.cfm" title="iLEARN Home Page">Mentoring Program</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/Education/postdoctoral-training/Pages/default.aspx" title="Postdoctoral Training">Postdoctoral Training</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/Education/opti-clearinghouse/Pages/default.aspx" title="OPTI Clearinghouse">OPTI Clearinghouse</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/Education/OGME-development-initiative/Pages/default.aspx" title="OGME Development Initiative">OGME Development</a>
-              </li>
-            </ul>
-          </div> <!-- .size-1-of-3 -->
+            <div class="footer__col">
+              <h3 class="footer__title">Advocacy</h3>
+              <ul class="footer__list">
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/public-policy/federal-legislative-priorities/Pages/default.aspx" title="Federal Legislative Priorities">Legislative Priorities</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/public-policy/regulatory-issues/Pages/default.aspx" title="Regulatory Issues">Regulatory Issues</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/advocacy/Pages/do-advocacy-action-center.aspx" title="Write to Congress">Write to Congress</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/public-policy/state-government-affairs/Pages/default.aspx" title="State Government Affairs">State Government Affairs</a>
+                </li>
+              </ul>
+              
+              <h3 class="footer__title">Education</h3>
+              <ul class="footer__list">
+                <li class="footer__list-item">
+                  <a href="http://opportunities.osteopathic.org/index.htm" title="Opportunities.osteopathic.org">Find a Training Program</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://cf.osteopathic.org/iLearn/home.cfm" title="iLEARN Home Page">Mentoring Program</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/Education/postdoctoral-training/Pages/default.aspx" title="Postdoctoral Training">Postdoctoral Training</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/Education/opti-clearinghouse/Pages/default.aspx" title="OPTI Clearinghouse">OPTI Clearinghouse</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/Education/OGME-development-initiative/Pages/default.aspx" title="OGME Development Initiative">OGME Development</a>
+                </li>
+              </ul>
+            </div> <!-- .footer__col -->
 
-          <div class="size-1-of-3--last">
-            <h3 class="footer__title">DO Profiles</h3>
-            <ul class="footer__list">
-              <li class="footer__list-item">
-                <a href="https://www.doprofiles.org/sign_in.cfm" title="AOIA - Account Sign In">Order Physician Credentialing Reports</a>
-              </li>
-            </ul>
-            
-            <h3 class="footer__title">Professional Development</h3>
-            <ul class="footer__list">
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/development/practice-mgt/Pages/default.aspx" title="Practice Management">Practice Management</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/development/do-jobs-online/Pages/default.aspx" title="DO Jobs Online">DO Jobs Online</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/development/quality/Pages/default.aspx" title="Quality and Research">Quality and Research</a>
-              </li>
-              <li class="footer__list-item">
-                <a href="http://www.osteopathic.org/inside-aoa/development/public-health/Pages/default.aspx" title="Public Health Initiatives">Public Health</a>
-              </li>
-            </ul>
-            
-            <h3 class="footer__title">Other publications</h3>
-            <ul class="footer__list">
-              <li class="footer__list-item"><a href="http://www.do-online.org/JAOA/JAOA.cfm" title="The Journal of the American Osteopathic Association">JAOA</a>
-              </li>
-            </ul>
-          </div><!-- .size-1-of-3-last -->
-        </div><!-- .row -->
+            <div class="footer__col--last">
+              <h3 class="footer__title">DO Profiles</h3>
+              <ul class="footer__list">
+                <li class="footer__list-item">
+                  <a href="https://www.doprofiles.org/sign_in.cfm" title="AOIA - Account Sign In">Order Physician Credentialing Reports</a>
+                </li>
+              </ul>
+              
+              <h3 class="footer__title">Professional Development</h3>
+              <ul class="footer__list">
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/development/practice-mgt/Pages/default.aspx" title="Practice Management">Practice Management</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/development/do-jobs-online/Pages/default.aspx" title="DO Jobs Online">DO Jobs Online</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/development/quality/Pages/default.aspx" title="Quality and Research">Quality and Research</a>
+                </li>
+                <li class="footer__list-item">
+                  <a href="http://www.osteopathic.org/inside-aoa/development/public-health/Pages/default.aspx" title="Public Health Initiatives">Public Health</a>
+                </li>
+              </ul>
+              
+              <h3 class="footer__title">Other publications</h3>
+              <ul class="footer__list">
+                <li class="footer__list-item"><a href="http://www.do-online.org/JAOA/JAOA.cfm" title="The Journal of the American Osteopathic Association">JAOA</a>
+                </li>
+              </ul>
+            </div><!-- .footer__col-last -->
+          </div><!-- .row -->
+        </div> <!-- .footer__container -->
 
-        <div class="row">
-          <div class="size-1-of-2">
-            <p class="footer__body-text">
-              Copyright <?php echo date('Y'); ?>, American Osteopathic Association. All rights reserved.
-            </p>
-          </div>
-          <div class="size-1-of-2--last">
-            <ul class="footer__list--horiz pull-right">
-              <li class="footer__list-item--horiz">
-                <a href="#">Terms of Use</a>
-              </li>
-              <li class="footer__list-item--horiz">
-                <a href="#">Privacy policy</a>
-              </li>
-              <li class="footer__list-item--horiz">
-                <a href="#">Copyright Notice</a>
-              </li>
-            </ul>
-          </div>
+        <div class="row--full module">
+          <p class="footer__body-text">
+            Copyright <?php echo date('Y'); ?>, American Osteopathic Association. <span>All rights reserved.</span>
+          </p>
+          <ul class="footer__list--horiz">
+            <li class="footer__list-item--horiz">
+              <a href="http://www.osteopathic.org/inside-aoa/about/Pages/terms-of-use-agreement.aspx" title="Terms of Use Agreement">Terms of Use</a>
+            </li>
+            <li class="footer__list-item--horiz">
+              <a href="http://www.osteopathic.org/inside-aoa/about/Pages/privacy-policy.aspx" title="Privacy Policy">Privacy Policy</a>
+            </li>
+            <li class="footer__list-item--horiz">
+              <a href="http://www.osteopathic.org/inside-aoa/about/Pages/copyright-notice.aspx" title="Copyright Notice">Copyright Notice</a>
+            </li>
+          </ul>
+
         </div><!-- .row -->
       </footer><!-- #colophon -->
     <script src="http://localhost:35729/livereload.js"></script>
