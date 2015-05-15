828033e49c8f00a51aaaaa27b2a44e2a286bdbbb
pjsinco <psinco@fas.harvard.edu>
2015-05-15 13:01:54 -0500

    add an .article-wrapper div

--- sidebar-article.php
+++ sidebar-article.php
@@ -10,7 +10,10 @@
         <aside data-set="rover-don-parent" class="ad rover-don-parent-b"></aside>
         <aside data-set="rover-peggy-parent" class="ad rover-peggy-parent-b"></aside>
 
-        <div class='widgets--article__wrapper'>
-        <?php if ( !dynamic_sidebar( 'article-sidebar' ) ); ?>
+        <div class="article-wrapper">
+          
+          <div class='widgets--article__wrapper'>
+          <?php if ( !dynamic_sidebar( 'article-sidebar' ) ); ?>
+          </div>
         </div>
 
