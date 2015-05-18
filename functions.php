dde100be24d4a10d3fcb92986fe0ff7344304d36
pjsinco <psinco@fas.harvard.edu>
2015-05-18 16:23:55 -0500

    remove gallery post-format support

--- functions.php
+++ functions.php
@@ -54,7 +54,7 @@ function elit_setup() {
 	add_theme_support( 'html5', array(
 		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
 	) );
-  add_theme_support( 'post-formats', array( 'video', 'gallery' ) );
+  add_theme_support( 'post-formats', array( 'video', ) );
 
   /*
    * Set our image sizes
