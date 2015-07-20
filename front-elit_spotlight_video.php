9c596a701d832f6b47ac958e39560f477c222fba
pjsinco <psinco@fas.harvard.edu>
2015-07-20 16:15:49 -0500

    change spotlight video to HOD video

--- front-elit_spotlight_video.php
+++ front-elit_spotlight_video.php
@@ -17,12 +17,12 @@
         <div class="unit size-1-of-1 module">
           <div id="spotlight" class="spotlight">
             <div class="spotlight__feature-wrapper elit-video" id="video">
-              <iframe width="654" height="368" src="https://www.youtube.com/embed/WFCe1_lUDVA?color=white&amp;autohide=1&amp;modestbranding=1" frameborder="0" allowfullscreen></iframe>
+              <iframe width="654" height="368" src="https://www.youtube.com/embed/v2ZWlUte7mk?color=white&amp;autohide=1&amp;modestbranding=1" frameborder="0" allowfullscreen></iframe>
             </div>
             <div class="spotlight__body">
-              <h5 class="spotlight__kicker">Finding patterns</h5>
-              <h2 class="spotlight__head"><a href="<?php echo get_permalink( 183025 ); ?>">A glimpse into the mind of a data-driven physician</a></h2>
-              <p class="spotlight__body-text">Jay Bhatt, DO, describes how physicians can use big-picture data from the community to provide better, more efficient care in the clinic. Dr. Bhatt is a former chief strategy and information officer for the Chicago Department of Public Health. <span class="spotlight__note"><a class="spotlight__link" href="<?php echo get_permalink( 183025 ); ?>">Read more &raquo;</a></span></p>
+              <h5 class="spotlight__kicker">Transforming care</h5>
+              <h2 class="spotlight__head">&lsquo;The time is now&rsquo;</h2>
+              <p class="spotlight__body-text">&ldquo;DOs have an extraordinary opportunity to influence health care in this historic moment,&rdquo; says Jay Bhatt, DO, MPH, in this video, which kicked off Day 2 of the AOA Annual Business Meeting.</p>
 
             </div>
           </div>
