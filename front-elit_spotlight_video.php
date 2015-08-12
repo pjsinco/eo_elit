8f24dbd2fdf41c51e8c2a7a534c4c0a61f353326
pjsinco <psinco@fas.harvard.edu>
2015-08-12 10:01:14 -0500

    Pull in first HOD 2015 video for spotlight

--- front-elit_spotlight_video.php
+++ front-elit_spotlight_video.php
@@ -17,12 +17,13 @@
         <div class="unit size-1-of-1 module">
           <div id="spotlight" class="spotlight">
             <div class="spotlight__feature-wrapper elit-video" id="video">
-              <iframe width="654" height="368" src="https://www.youtube.com/embed/v2ZWlUte7mk?color=white&amp;autohide=1&amp;modestbranding=1" frameborder="0" allowfullscreen></iframe>
+              <iframe width="654" height="368" src="https://www.youtube.com/embed/uo2OC6ExGSg?color=white&amp;autohide=1&amp;modestbranding=1" frameborder="0" allowfullscreen></iframe>
             </div>
             <div class="spotlight__body">
-              <h5 class="spotlight__kicker">Transforming care</h5>
-              <h2 class="spotlight__head">&lsquo;The time is now&rsquo;</h2>
-              <p class="spotlight__body-text">&ldquo;DOs have an extraordinary opportunity to influence health care in this historic moment,&rdquo; says Jay Bhatt, DO, MPH, in this video, which kicked off Day 2 of the AOA Annual Business Meeting.</p>
+              <h5 class="spotlight__kicker">Hindsight is 20-20</h5>
+              <h2 class="spotlight__head">What do you wish you had known?</h2>
+              <p class="spotlight__body-text">Looking back on their medical careers, DOs say they wish they had known how much fun they were going to have, how many friends they were going to make in the field and how to participate in developing policy.</p>
+              <p class="spotlight__body-text">&ldquo;You think you're going to be a doctor and it's going to be a very rewarding career, but I learn every day the difference a DO makes,&rdquo; says Carol Henwood, DO.</p>
 
             </div>
           </div>
