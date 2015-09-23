cad532420de3f07da4c4e01f210701e9cf4c12a8
pjsinco <psinco@fas.harvard.edu>
2015-09-23 17:25:44 -0500

    Swap in spotlight video 2015-09-23

--- front-elit_spotlight_video.php
+++ front-elit_spotlight_video.php
@@ -17,13 +17,12 @@
         <div class="unit size-1-of-1 module">
           <div id="spotlight" class="spotlight">
             <div class="spotlight__feature-wrapper elit-video" id="video">
-              <iframe width="654" height="368" src="https://www.youtube.com/embed/uo2OC6ExGSg?color=white&amp;autohide=1&amp;modestbranding=1" frameborder="0" allowfullscreen></iframe>
+              <iframe width="654" height="368" src="https://www.youtube.com/embed/LRy9dRxf69Q?color=white&amp;autohide=1&amp;modestbranding=1" frameborder="0" allowfullscreen></iframe>
             </div>
             <div class="spotlight__body">
-              <h5 class="spotlight__kicker">Hindsight is 20-20</h5>
-              <h2 class="spotlight__head">What do you wish you had known?</h2>
-              <p class="spotlight__body-text">Looking back on their medical careers, DOs say they wish they had known how much fun they were going to have, how many friends they were going to make in the field and how to participate in developing policy.</p>
-              <p class="spotlight__body-text">&ldquo;You think you're going to be a doctor and it's going to be a very rewarding career, but I learn every day the difference a DO makes,&rdquo; says Carol Henwood, DO.</p>
+              <h5 class="spotlight__kicker">A life-changing decision</h5>
+              <h2 class="spotlight__head">Why did you choose osteopathic medicine?</h2>
+              <p class="spotlight__body-text">Some DOs became osteopathic physicians because they grew up with osteopathic medicine. Others were drawn to the profession's emphasis on patient-centered care, empathy and osteopathic manipulative treatment.</p>
 
             </div>
           </div>
