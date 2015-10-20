08694dd1b13e645ec6cdfab6daf97c2b887a9b85
pjsinco <psinco@fas.harvard.edu>
2015-10-19 20:20:38 -0400

    Swap in new spotlight video

--- front-elit_spotlight_video.php
+++ front-elit_spotlight_video.php
@@ -17,12 +17,12 @@
         <div class="unit size-1-of-1 module">
           <div id="spotlight" class="spotlight">
             <div class="spotlight__feature-wrapper elit-video" id="video">
-              <iframe width="654" height="368" src="https://www.youtube.com/embed/LRy9dRxf69Q?color=white&amp;autohide=1&amp;modestbranding=1" frameborder="0" allowfullscreen></iframe>
+              <iframe width="654" height="368" src="https://www.youtube.com/embed/ac2Z9-7ftCU?color=white&amp;autohide=1&amp;modestbranding=1" frameborder="0" allowfullscreen></iframe>
             </div>
             <div class="spotlight__body">
-              <h5 class="spotlight__kicker">A life-changing decision</h5>
-              <h2 class="spotlight__head">Why did you choose osteopathic medicine?</h2>
-              <p class="spotlight__body-text">Some DOs became osteopathic physicians because they grew up with osteopathic medicine. Others were drawn to the profession's emphasis on patient-centered care, empathy and osteopathic manipulative treatment.</p>
+              <h5 class="spotlight__kicker">'Really insprired'</h5>
+              <h2 class="spotlight__head">What do you think about the new brand campaign?</h2>
+              <p class="spotlight__body-text">DOs at OMED 2015 react to the unveiling of the AOA's <a href="/2015/10/people-want-doctors-who-listen-creating-a-national-awareness-campaign/">new national awareness campaign</a>. "I really believe that DOs care about the whole person," says Kelli Glaser, DO, of Phoenix. "And I think the campaign is going to demonstrate that."</p>
 
             </div>
           </div>
