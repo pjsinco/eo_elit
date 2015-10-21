65c7c8cb980a073d7b39e9565055ef9e031f0c5f
pjsinco <psinco@gmail.com>
2015-10-21 09:02:11 -0500

    Tweak language in spotlight video

--- front-elit_spotlight_video.php
+++ front-elit_spotlight_video.php
@@ -20,9 +20,9 @@
               <iframe width="654" height="368" src="https://www.youtube.com/embed/ac2Z9-7ftCU?color=white&amp;autohide=1&amp;modestbranding=1" frameborder="0" allowfullscreen></iframe>
             </div>
             <div class="spotlight__body">
-              <h5 class="spotlight__kicker">'Really insprired'</h5>
-              <h2 class="spotlight__head">What do you think about the new brand campaign?</h2>
-              <p class="spotlight__body-text">DOs at OMED 2015 react to the unveiling of the AOA's <a href="/2015/10/people-want-doctors-who-listen-creating-a-national-awareness-campaign/">new national awareness campaign</a>. "I really believe that DOs care about the whole person," says Kelli Glaser, DO, of Phoenix. "And I think the campaign is going to demonstrate that."</p>
+              <h5 class="spotlight__kicker">'Really inspired'</h5>
+              <h2 class="spotlight__head">What do you think of the new brand campaign?</h2>
+              <p class="spotlight__body-text">DOs and students at OMED 2015 react to the unveiling of the AOA's <a href="/2015/10/people-want-doctors-who-listen-creating-a-national-awareness-campaign/">new national awareness campaign</a>. "I do really believe that DOs care about the whole person," says Kelli Glaser, DO, of Phoenix. "And I think the campaign is going to demonstrate that."</p>
 
             </div>
           </div>
