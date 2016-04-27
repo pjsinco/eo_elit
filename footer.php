3c2da07a16526309f5bdfe2fcc372f648626ed9a
pjsinco <psinco@gmail.com>
2016-04-27 10:24:33 -0500

    Make new styles for footer; get contact-us prototype in footer

--- footer.php
+++ footer.php
@@ -10,21 +10,32 @@
 
       <footer id="colophon" class="footer" role="contentinfo">
         <div class="footer__body">
-          <div class="row--full">
-          <?php
-            if ( function_exists( 'ninja_forms_display_form' ) ) {
-              ninja_forms_display_form( 5 );
-            } 
-          ?>
-            <h2 class="footer__header">Got a news tip?</h2>
+          <div class="row--full module--footer">
+            <div class="size-1-of-2">
+            <?php
+              if ( function_exists( 'ninja_forms_display_form' ) ) {
+                ninja_forms_display_form( 5 );
+              } 
+            ?>
+            </div>
+            <div class="size-1-of-2--last">
+              <h2 class="footer__header--minor">Got a news tip?</h2>
+              <p class="footer__body-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.
+              <p>
+              <p class="footer__body-text">
+                <button class="footer__btn"><a href="#">Let us know</a></button>
+              <p>
+            </div>
           </div>
           <div class="row--full">
-            <h2 class="footer__header">More from the <span><a href="http://www.osteopathic.org/Pages/default.aspx" title="American Osteopathic Association">American Osteopathic Association</a></span></h2>
+            <h2 class="footer__header--minor">More from the <span><a href="http://www.osteopathic.org/Pages/default.aspx" title="American Osteopathic Association">American Osteopathic Association</a></span></h2>
             <div class="footer__col">
               <h3 class="footer__title">About the AOA</h3>
               <ul class="footer__list">
                 <li class="footer__list-item">
-                  <a class="footer__list-item"><a href="http://doctorsthatdo.org/" title="Home - Doctors of Osteopathic Medicine">Doctors That DO</a></li>
+                  <a href="http://doctorsthatdo.org/" title="Home - Doctors of Osteopathic Medicine">Doctors That DO</a>
+                </li>
                 <li class="footer__list-item">
                   <a href="http://www.osteopathic.org/inside-aoa/about/aoa-membership/Pages/default.aspx" title="Enjoy the Benefits of Being an AOA Member">AOA Membership</a>
                 </li>
