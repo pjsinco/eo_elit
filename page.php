c65d2c29f7a0c21361314d3baa2f47133d1bd859
pjsinco <psinco@gmail.com>
2016-10-11 10:40:46 -0500

    Use new section classes on page.php, page-contact.php
    These are the layout classes: '.one-col' and '.two-col'

--- page.php
+++ page.php
@@ -1,8 +1,9 @@
 <?php get_header(); ?>
 
+<?php $layout = 'two-col'; ?>
 
     <div id="main" class="content">
-      <section id="primary" class="content__primary">
+      <section id="primary" class="content__primary--<?php echo $layout; ?>">
 
         <?php while(have_posts()): the_post(); ?>
 
@@ -12,7 +13,7 @@
 
       </section> <!-- #primary -->
 
-      <section id="secondary" class="content__secondary">
+      <section id="secondary" class="<?php elit_secondary_class( $layout ); ?>">
 
         <?php get_sidebar('article'); ?>
 
