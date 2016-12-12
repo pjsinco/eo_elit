76dcba1d890fc1bece9266bcd69fd382f2db1593
pjsinco <psinco@gmail.com>
2016-12-12 10:00:45 -0600

    Update C3 scripts for post #196768

--- js/c3-temp/script-loan-forgiveness.js
+++ js/c3-temp/script-loan-forgiveness.js
@@ -2,11 +2,11 @@
 var chart = c3.generate({
     data: {
         columns: [
-            ['First monthly payment', 2271, 1306, 1350, 1083, 310, 310, 310, 465, 719],
-            ['Last monthly payment', 2271, 3917, 1350, 1966, 1219, 928, 928, 1828, 2611]
+        ['Total amount paid', 272515, 292291, 405124, 439585, 203039, 137303, 137303, 304640, 445790],
+        ['Projected loan forgiveness*', 0, 0, 0, 0, 259652, 322697, 322697, 220360, 69484]  
         ],
 
-        type: 'bar',
+        // type: 'bar',
         types: {
             'Total amount paid': 'spline',
             'Projected loan forgiveness*': 'spline'
@@ -15,25 +15,25 @@ var chart = c3.generate({
         colors: {
             'Total amount paid': '#ef3f23',
             'Projected loan forgiveness*' : 'gray',
-            'First monthly payment': '#a6bbc8',
-            'Last monthly payment': 'black'
+            // 'First monthly payment': '#a6bbc8',
+            // 'Last monthly payment': 'black'
         },
         color:function(color,d){
             return d.id && d.id ==='data3' ? d3.rgb(color).darker(d.value/150) :color;
         }
     },
-    bar: {
-        width: {
-            ratio: 0.5 // this makes bar width 50% of length between ticks
-        }
-        // or
-        //width: 100 // this makes bar width 100px
-    },
+    // bar: {
+    //     width: {
+    //         ratio: 0.5 // this makes bar width 50% of length between ticks
+    //     }
+    //     // or
+    //     //width: 100 // this makes bar width 100px
+    // },
     axis: {
         x:{
             label: {
                 text:'Repayment plan',
-                position: 'inner-left'
+                position: 'inner-right'
                     },
             type: 'category',
             tick: {
@@ -43,7 +43,7 @@ var chart = c3.generate({
             categories: [ 'Standard','Graduated','Extended Fixed', 
             'Extended Graduated', 'REPAYE',
             'PAYE', 'IBR for New Borrowers',
-            'IBR','ICIR']
+            'IBR','ICR']
         },
         y:{
             label:{
@@ -61,14 +61,14 @@ var chart = c3.generate({
         },
 });
 
-setTimeout(function () {
-    chart.load({
-        columns: [
-        ['Total amount paid', 272515, 292291, 405124, 439585, 203039, 137303, 137303, 304640, 445790],
-        ['Projected loan forgiveness*', 0, 0, 0, 0, 259652, 322697, 322697, 220360, 69484]  
-            ]
-    });
-}, 5000);
+// setTimeout(function () {
+//     chart.load({
+//         columns: [
+//         ['Total amount paid', 272515, 292291, 405124, 439585, 203039, 137303, 137303, 304640, 445790],
+//         ['Projected loan forgiveness*', 0, 0, 0, 0, 259652, 322697, 322697, 220360, 69484]  
+//             ]
+//     });
+// }, 5000);
 
 
 
---
+++ js/c3-temp/script-monthly-comparison.js
@@ -0,0 +1,68 @@
+
+var chart = c3.generate({
+    data: {
+        columns: [
+            ['First monthly payment', 2271, 1306, 1350, 1083, 310, 310, 310, 465, 719],
+        ],
+
+        type: 'bar',
+        colors: {
+            'First monthly payment': '#ef3f23',
+            'Last monthly payment': 'gray'
+        },
+        color:function(color,d){
+            return d.id && d.id ==='data3' ? d3.rgb(color).darker(d.value/150) :color;
+        }
+        
+    },
+    bar: {
+        width: {
+            ratio: 0.5 // this makes bar width 50% of length between ticks
+        }
+        // or
+        //width: 100 // this makes bar width 100px
+    },
+    axis: {
+        x:{
+            label: {
+                text:'Repayment plan',
+                position: 'inner-left'
+                    },
+                    tick:{
+            rotate: 75,
+            multiline: false
+            },
+            type: 'category',
+            categories: [ 'Standard','Graduated','Extended Fixed', 
+            'Extended Graduated', 'REPAYE',
+            'PAYE', 'IBR for New Borrowers',
+            'IBR','ICR']
+        },
+
+        y:{
+            label:{
+                text:'Dollar amount',
+                position: 'outer-middle'
+                  },
+            tick:{
+                format: d3.format("$,")  
+                }
+           } 
+        },
+
+        legend:{
+            position: 'inset',
+        },
+
+ 
+            
+
+});
+
+setTimeout(function () {
+    chart.load({
+        columns: [
+        ['Last monthly payment', 2271, 3917, 1350, 1966, 1219, 928,928,1828, 2611]  
+            ]
+    });
+}, 2000);
