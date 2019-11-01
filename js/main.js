c4df763b01c3f905d807f0455e87e8979fc38df5
Patrick Sinco <psinco@gmail.com>
2019-11-01 12:17:17 -0500

    Fix problem with google ads call to refresh

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
 jQuery(document).ready(function() {
-
   /**
    * nav.js
    */
-  
+
   // menu is hidden by default to keep from showing
   // during page load; so we need to make it seeable
   // after the page loads
   $('.nav').show();
 
   $('body').addClass('js');
-  var $menu = $('#site-nav'), 
+  var $menu = $('#site-nav'),
     $menulink = $('.nav__link--toggle');
 
-
-  $menulink.click(function(evt) { 
-    $menulink.toggleClass('active'); 
-    $menu.toggleClass('active') ;
+  $menulink.click(function(evt) {
+    $menulink.toggleClass('active');
+    $menu.toggleClass('active');
     return false;
     //evt.preventDefault();
   });
@@ -25,11 +23,11 @@ jQuery(document).ready(function() {
    * appendAround
    *
    */
-  jQuery(".rover-don").appendAround();
-  jQuery(".rover-peggy").appendAround();
+  jQuery('.rover-don').appendAround();
+  jQuery('.rover-peggy').appendAround();
 
   $(window).resize(function() {
-    if (window.googletag) {
+    if (window.googletag && window.googletag.pubadsReady) {
       window.googletag.pubads().refresh();
     }
   });
@@ -42,45 +40,60 @@ jQuery(document).ready(function() {
    *
    */
   function windowPopup(url, width, height) {
-    // Calculate the position of the popup 
+    // Calculate the position of the popup
     // so it's centered
-    var left = (screen.left / 2) - (width / 2 ),
-      top = (screen.top / 2) - (height / 2 );
+    var left = screen.left / 2 - width / 2,
+      top = screen.top / 2 - height / 2;
 
     window.open(
       url,
       '',
       'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=' +
-        width + ',height=' + height + ',top=' + top + ',left=' + left
+        width +
+        ',height=' +
+        height +
+        ',top=' +
+        top +
+        ',left=' +
+        left
     );
   }
 
-  jQuery('#social-facebook, #social-twitter, #social-linkedin, #social-pinterest')
-    .on('click', function(evt) {
-      evt.preventDefault();
-      windowPopup($(this).attr('href'), 500, 300);
-    })
+  jQuery(
+    '#social-facebook, #social-twitter, #social-linkedin, #social-pinterest'
+  ).on('click', function(evt) {
+    evt.preventDefault();
+    windowPopup($(this).attr('href'), 500, 300);
+  });
 
   if (window.location.href.indexOf('show_comment_policy=true') > -1) {
-    vex.defaultOptions.className = "vex-theme-default";
-    vex.dialog.alert({ 
-      unsafeMessage: '<h3>Thank you for your comment!</h3>' +
-                     'Please note that all comments are moderated. ' +
-                     'Review may take up to two business days.' +
-                     '<br><br>See the <a href="/comment-policy">' +
-                     'comment policy</a> for details.',
+    vex.defaultOptions.className = 'vex-theme-default';
+    vex.dialog.alert({
+      unsafeMessage:
+        '<h3>Thank you for your comment!</h3>' +
+        'Please note that all comments are moderated. ' +
+        'Review may take up to two business days.' +
+        '<br><br>See the <a href="/comment-policy">' +
+        'comment policy</a> for details.',
       afterClose: function() {
         // Remove 'show_comment_policy' query string
         // https://www.quora.com/How-do-I-remove-a-specific-key-value-pair-in-
         //         a-query-string-without-reloading-using-JavaScript
-        var href = window.location.href
-        window.location.href = href.split('?')
-          .map(function (url, i) {
-            return !i ? url : url.split('&').filter(function(p) {
-              return p.indexOf('show_comment_policy=') < 0;
-            }).join('&');
-          }).join('?');
-      }
+        var href = window.location.href;
+        window.location.href = href
+          .split('?')
+          .map(function(url, i) {
+            return !i
+              ? url
+              : url
+                  .split('&')
+                  .filter(function(p) {
+                    return p.indexOf('show_comment_policy=') < 0;
+                  })
+                  .join('&');
+          })
+          .join('?');
+      },
     });
   }
 });
